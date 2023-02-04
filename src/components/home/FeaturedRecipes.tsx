c00a1dbffc16d74376f2e2eb5872ee05d8f04deb
DanCristianIlie <ilie.dancrist@protonmail.com>
2023-02-04 16:39:44 +0200

    add favorite recipes to home page

--- src/components/home/FeaturedRecipes.tsx
+++ src/components/home/FeaturedRecipes.tsx
@@ -1,5 +1,6 @@
-FeaturedRecipes;
-/* This example requires Tailwind CSS v2.0+ */
+import RecipeCard from "@/components/recipes/RecipeCard";
+import { api } from "@/utils/api";
+
 const recipes = [
   {
     title: "Cereals with milk and berries",
@@ -55,6 +56,13 @@ const recipes = [
 ];
 
 export default function FeaturedRecipes() {
+  const { data, isLoading } =
+    api.recipe.getLimitedFavoritedRecipes.useInfiniteQuery({ limit: 3 });
+
+  console.log(data);
+
+  const recipesFilter = data?.pages[0]?.recipesWithDetails;
+
   return (
     <div className="relative bg-zinc-200 px-4 pt-16 pb-20 dark:bg-zinc-800 sm:px-6 lg:px-8 lg:pt-24 lg:pb-28">
       <div className="absolute inset-0">
@@ -70,59 +78,40 @@ export default function FeaturedRecipes() {
           </p>
         </div>
         <div className="mx-auto mt-12 grid max-w-lg gap-5 lg:max-w-none lg:grid-cols-3">
-          {recipes.map((post) => (
-            <div
-              key={post.title}
-              className="flex flex-col overflow-hidden rounded-lg shadow-lg"
-            >
-              <div className="flex-shrink-0">
-                <img
-                  className="h-48 w-full object-cover"
-                  src={post.imageUrl}
-                  alt=""
+          {!isLoading ? (
+            recipesFilter?.map((recipe) => (
+              <RecipeCard
+                key={recipe.id}
+                name={recipe.name}
+                description={recipe.description}
+                category={recipe.category[0]?.name as string}
+                imageSrc={recipe.image as string}
+                slug={recipe.slug}
+                userName={recipe.user?.name as string}
+                userImage={recipe.user?.image as string}
+              />
+            ))
+          ) : (
+            <div className="flex h-full w-full items-center justify-center">
+              <svg
+                aria-hidden="true"
+                className="mr-2 h-8 w-8 animate-spin fill-yellow-500 text-zinc-200 dark:text-zinc-600"
+                viewBox="0 0 100 101"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                  fill="currentColor"
                 />
-              </div>
-              <div className="flex flex-1 flex-col justify-between bg-zinc-100 p-6 dark:bg-zinc-700">
-                <div className="flex-1">
-                  <p className="text-lg font-medium text-yellow-600 dark:text-yellow-400">
-                    <a href={post.category.href} className="hover:underline">
-                      {post.category.name}
-                    </a>
-                  </p>
-                  <a href={post.href} className="mt-2 block">
-                    <p className="text-xl font-semibold text-zinc-900 dark:text-zinc-100">
-                      {post.title}
-                    </p>
-                    <p className="mt-3 text-base text-zinc-700 dark:text-zinc-300">
-                      {post.description}
-                    </p>
-                  </a>
-                </div>
-                <div className="mt-6 flex items-center">
-                  <div className="flex-shrink-0">
-                    <a href={post.author.href}>
-                      <span className="sr-only">{post.author.name}</span>
-                      <img
-                        className="h-10 w-10 rounded-full"
-                        src={post.author.imageUrl}
-                        alt=""
-                      />
-                    </a>
-                  </div>
-                  <div className="ml-3">
-                    <p className="text-sm font-medium text-zinc-900 dark:text-zinc-100">
-                      <a href={post.author.href} className="hover:underline">
-                        {post.author.name}
-                      </a>
-                    </p>
-                    <div className="flex space-x-1 text-sm text-zinc-600 dark:text-zinc-400">
-                      <time dateTime={post.datetime}>{post.date}</time>
-                    </div>
-                  </div>
-                </div>
-              </div>
+                <path
+                  d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                  fill="currentFill"
+                />
+              </svg>
+              <span className="sr-only">Loading...</span>
             </div>
-          ))}
+          )}
         </div>
       </div>
     </div>
--- src/server/api/routers/recipe.ts
+++ src/server/api/routers/recipe.ts
@@ -217,6 +217,78 @@ export const recipeRouter = createTRPCRouter({
       return { recipesWithDetails, nextCursor };
     }),
 
+  getLimitedFavoritedRecipes: publicProcedure
+    .input(
+      z.object({
+        limit: z.number().min(1).max(100),
+        cursor: z.string().nullish(),
+      })
+    )
+    .query(async ({ input, ctx }) => {
+      const limit = input.limit ?? 50;
+      const { cursor } = input;
+
+      const recipes = await ctx.prisma.recipe.findMany({
+        take: limit + 1,
+        where: {
+          shared: true,
+        },
+        orderBy: {
+          FavoriteRecipes: {
+            _count: "desc",
+          }
+        },
+        cursor: cursor ? { id: cursor } : undefined,
+      });
+      let nextCursor: typeof cursor | undefined = undefined;
+      if (recipes.length > limit) {
+        const nextItem = recipes.pop();
+        nextCursor = nextItem?.id;
+      }
+
+      const recipesWithDetails = await Promise.all(
+        recipes.map(async (recipe) => {
+          const ingredients = await ctx.prisma.ingredient.findMany({
+            where: {
+              recipeId: recipe.id,
+            },
+          });
+
+          const instructions = await ctx.prisma.instructions.findMany({
+            where: {
+              recipeId: recipe.id,
+            },
+          });
+
+          const category = await ctx.prisma.category.findMany({
+            where: {
+              recipes: {
+                some: {
+                  id: recipe.id,
+                },
+              },
+            },
+          });
+
+          const user = await ctx.prisma.user.findUnique({
+            where: {
+              id: recipe.userId,
+            },
+          });
+
+          return {
+            ...recipe,
+            ingredients,
+            instructions,
+            category,
+            user,
+          };
+        })
+      );
+
+      return { recipesWithDetails, nextCursor };
+    }),
+
   getRecipes: publicProcedure.query(async ({ ctx }) => {
     const recipes = await ctx.prisma.recipe.findMany({
       where: {
