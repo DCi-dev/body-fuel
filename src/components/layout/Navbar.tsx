ddaad5dcd7eb0e1797c6587fe9171ed1d02d32bb
CDi <ilie.dancrist@protonmail.com>
2023-03-01 13:52:17 +0200

    fix errors

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -259,9 +259,10 @@ export default function Navbar({ theme, setTheme, router, navRef}: NavbarProps) 
                     <Disclosure.Button
                       as="button"
                       className="block rounded-md px-3 py-2 text-base font-medium text-zinc-600 hover:bg-zinc-100 hover:text-black dark:text-zinc-100 dark:hover:bg-zinc-700 dark:hover:text-white"
-                      onClick={handleSignIn}
-                    >
+                      >
+                      <button onClick={handleSignIn}>
                         Sign in                     
+                      </button>
                     </Disclosure.Button>
                   </>
                 )}
