ef69d56d88c63e0fde465a2703f7d420de3e7209
CDi <github@cristd.cc>
2023-07-11 14:35:19 +0300

    add more trpc recipe route tests

--- src/server/api/routers/__tests__/recipe.test.ts
+++ src/server/api/routers/__tests__/recipe.test.ts
@@ -2,12 +2,13 @@
  * @jest-environment node
  */
 
-import type { FavoriteRecipes } from "@prisma/client";
 import { type RouterInputs } from "@utils/api";
 import {
   caller,
-  ctx,
   mockFavoriteRecipe,
+  mockGetRecipeResult,
+  mockIngredients,
+  mockInstructions,
   mockLimitedFavoritedRecipes,
   mockLimitedRecipes,
   mockRecipe,
@@ -23,7 +24,8 @@ jest.mock("aws-sdk", () => {
       createPresignedPost: jest
         .fn()
         .mockReturnValue("http://mock-presigned-url.com"),
-      // add other methods you want to mock here
+      deleteObject: jest.fn().mockReturnThis(),
+      promise: jest.fn(),
     })),
     config: {
       update: jest.fn(),
@@ -191,6 +193,180 @@ describe("recipeRouter", () => {
 
       // Expect the result to be the mockOutput
       expect(result).toEqual([mockRecipe]);
+    });
+  });
+
+  describe("getUserLimitedRecipes", () => {
+    const input = {
+      limit: 10,
+      cursor: null,
+      search: "test-search",
+    };
+    it("should get limited user recipes", async () => {
+      // make prisma.recipe.findMany return mockRecipe
+      prisma.recipe.findMany.mockResolvedValueOnce([mockRecipe]);
+
+      // Call the procedure
+      const result = await caller.recipe.getUserLimitedRecipes(input);
+
+      // Expect the result to be the mockOutput
+      expect(result).toEqual(mockLimitedFavoritedRecipes);
+    });
+  });
+
+  describe("getRecipe", () => {
+    const input = {
+      slug: "test-recipe-slug",
+    };
+
+    it("should get recipe", async () => {
+      // make prisma.recipe.findUnique return mockRecipe
+      prisma.recipe.findUnique.mockResolvedValueOnce(mockRecipe);
+
+      // make prisma.ingredients.findMany return mockIngredients
+      prisma.ingredient.findMany.mockResolvedValueOnce(mockIngredients);
+
+      // make prisma.instructions.findMany return mockInstructions
+      prisma.instructions.findMany.mockResolvedValueOnce(mockInstructions);
+
+      // make prisma.user.findUnique return userSession.user
+      prisma.user.findUnique.mockResolvedValueOnce(userSession.user);
+
+      // make prisma.review.findMany return mockReview
+      prisma.review.findMany.mockResolvedValueOnce([mockReview]);
+
+      // Call the procedure
+      const result = await caller.recipe.getRecipe(input);
+
+      // Expect the result to be the mockOutput
+      expect(result).toEqual(mockGetRecipeResult);
+    });
+
+    it("should throw an error if recipe is not found", async () => {
+      // make prisma.recipe.findUnique return null
+      prisma.recipe.findUnique.mockResolvedValueOnce(null);
+
+      // Call the procedure
+      const result = caller.recipe.getRecipe(input);
+
+      // Expect the result to throw an error
+      await expect(result).rejects.toThrowError("Recipe not found");
+    });
+  });
+
+  describe("getUserFavoriteRecipesIds", () => {
+    it("should get user favorite recipes ids", async () => {
+      // make prisma.favoriteRecipes.findMany return mockFavoriteRecipe
+      prisma.favoriteRecipes.findMany.mockResolvedValueOnce([
+        mockFavoriteRecipe,
+      ]);
+
+      // Call the procedure
+      const result = await caller.recipe.getUserFavoriteRecipesIds();
+
+      // Expect the result to be the mockOutput
+      expect(result).toEqual([mockFavoriteRecipe.recipeId]);
+    });
+  });
+
+  describe("getLimitedUserFavoriteRecipes", () => {
+    const input = {
+      limit: 10,
+      cursor: null,
+      search: "test-search",
+    };
+    it("should get limited user favorite recipes", async () => {
+      // make prisma.favoriteRecipes.findMany return mockFavoriteRecipe
+      prisma.favoriteRecipes.findMany.mockResolvedValueOnce([
+        mockFavoriteRecipe,
+      ]);
+
+      // make prisma.recipe.findMany return mockRecipe
+      prisma.recipe.findMany.mockResolvedValueOnce([mockRecipe]);
+
+      // Call the procedure
+      const result = await caller.recipe.getLimitedUserFavoriteRecipes(input);
+
+      // Expect the result to be the mockOutput
+      expect(result).toEqual(mockLimitedFavoritedRecipes);
+    });
+  });
+
+  describe("deleteRecipe", () => {
+    const input = "test-recipe-id";
+
+    it("should delete recipe", async () => {
+      // make prisma.recipe.findUnique return mockRecipe
+      prisma.recipe.findUnique.mockResolvedValueOnce(mockRecipe);
+
+      // make prisma.recipe.delete return mockRecipe
+      prisma.recipe.delete.mockResolvedValueOnce(mockRecipe);
+
+      // Call the procedure
+      const result = await caller.recipe.deleteRecipe(input);
+
+      // Expect the result to be the mockOutput
+      expect(result).toEqual(mockRecipe);
+    });
+
+    it("should throw an error if recipe is not found", async () => {
+      // make prisma.recipe.findUnique return null
+      prisma.recipe.findUnique.mockResolvedValueOnce(null);
+
+      // Call the procedure
+      const result = caller.recipe.deleteRecipe(input);
+
+      // Expect the result to throw an error
+      await expect(result).rejects.toThrowError("Recipe not found");
+    });
+
+    it("should throw an error if the recipe does not belong to the user", async () => {
+      // make prisma.recipe.findUnique return a recipe with different userId
+      const unauthorizedRecipe = { ...mockRecipe, userId: "different-user-id" };
+      prisma.recipe.findUnique.mockResolvedValueOnce(unauthorizedRecipe);
+
+      // Call the procedure
+      const result = caller.recipe.deleteRecipe(input);
+
+      // Expect the result to throw an error
+      await expect(result).rejects.toThrowError(
+        "You are not allowed to delete this recipe"
+      );
+    });
+  });
+
+  describe("updateSharedRecipe", () => {
+    const input = {
+      id: "test-recipe-id",
+      shared: true,
+    };
+
+    it("should update shared recipe", async () => {
+      // make prisma.recipe.findUnique return mockRecipe
+      prisma.recipe.findUnique.mockResolvedValueOnce(mockRecipe);
+
+      // make prisma.recipe.update return mockRecipe
+      prisma.recipe.update.mockResolvedValueOnce(mockRecipe);
+
+      // Call the procedure
+      const result = await caller.recipe.updateSharedRecipe(input);
+
+      // Expect the result to be the mockOutput
+      expect(result).toEqual(mockRecipe);
+    });
+
+    it("should throw an error if the recipe does not belong to the user", async () => {
+      // make prisma.recipe.findUnique return a recipe with different userId
+      const unauthorizedRecipe = { ...mockRecipe, userId: "different-user-id" };
+      prisma.recipe.findUnique.mockResolvedValueOnce(unauthorizedRecipe);
+
+      // Call the procedure
+      const result = caller.recipe.updateSharedRecipe(input);
+
+      // Expect the result to throw an error
+      await expect(result).rejects.toThrowError(
+        "You are not allowed to update this recipe"
+      );
     });
   });
 });
--- src/server/api/routers/helpers/tests.ts
+++ src/server/api/routers/helpers/tests.ts
@@ -1,6 +1,7 @@
 import type {
   FavoriteRecipes,
   Ingredient,
+  Instructions,
   PrismaClient,
   Recipe,
   Review,
@@ -54,6 +55,20 @@ export const mockIngredients: Ingredient[] = [
     carbohydrates: 20,
     fat: 20,
     protein: 20,
+  },
+];
+
+// Mock instructions
+export const mockInstructions: Instructions[] = [
+  {
+    text: "test-instruction-text-1",
+    id: "test-instruction-id-1",
+    recipeId: "test-recipe-id",
+  },
+  {
+    text: "test-instruction-text-2",
+    id: "test-instruction-id-2",
+    recipeId: "test-recipe-id",
   },
 ];
 
@@ -121,6 +136,7 @@ export const mockLimitedRecipes = {
   ],
 };
 
+// Mock limitedFavoritedRecipes
 export const mockLimitedFavoritedRecipes = {
   nextCursor: undefined,
   recipesWithDetails: [
@@ -146,4 +162,94 @@ export const mockLimitedFavoritedRecipes = {
       userId: "test-user-id",
     },
   ],
+};
+
+// Mock recipe that is not shared
+export const mockRecipeNotShared: Recipe = {
+  id: "test-recipe-id",
+  name: "test-recipe-name",
+  description: "test-recipe-description",
+  servings: 10,
+  image: "test-recipe-imageUrl",
+  userId: "test-user-id",
+  calories: 300,
+  carbohydrates: 30,
+  fat: 30,
+  protein: 30,
+  cookTime: "30 min",
+  prepTime: "30 min",
+  category: "Breakfast",
+  shared: false,
+  difficulty: "Easy",
+  slug: "test-recipe-slug",
+};
+
+// Mock getRecipe result
+export const mockGetRecipeResult = {
+  calories: 300,
+  carbohydrates: 30,
+  category: "Breakfast",
+  cookTime: "30 min",
+  description: "test-recipe-description",
+  difficulty: "Easy",
+  fat: 30,
+  id: "test-recipe-id",
+  image: "test-recipe-imageUrl",
+  ingredients: [
+    {
+      calories: 100,
+      carbohydrates: 10,
+      fat: 10,
+      id: "test-ingredient-id-1",
+      name: "test-ingredient-name-1",
+      protein: 10,
+      quantity: 10,
+      recipeId: "test-recipe-id",
+    },
+    {
+      calories: 200,
+      carbohydrates: 20,
+      fat: 20,
+      id: "test-ingredient-id-2",
+      name: "test-ingredient-name-2",
+      protein: 20,
+      quantity: 20,
+      recipeId: "test-recipe-id",
+    },
+  ],
+  instructions: [
+    {
+      id: "test-instruction-id-1",
+      recipeId: "test-recipe-id",
+      text: "test-instruction-text-1",
+    },
+    {
+      id: "test-instruction-id-2",
+      recipeId: "test-recipe-id",
+      text: "test-instruction-text-2",
+    },
+  ],
+  name: "test-recipe-name",
+  prepTime: "30 min",
+  protein: 30,
+  reviewWithUserDetails: [
+    {
+      comments: "test-review-comments",
+      id: "test-review-id",
+      recipeId: "test-recipe-id",
+      stars: 5,
+      user: undefined,
+      userId: "test-user-id",
+    },
+  ],
+  servings: 10,
+  shared: true,
+  slug: "test-recipe-slug",
+  user: {
+    email: "test-user-email",
+    id: "test-user-id",
+    image: "test-user-image",
+    name: "test-user-name",
+  },
+  userId: "test-user-id",
 };
